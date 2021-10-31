cb7e1d51d4d626952e2b1645f16d6a37d6f1ae43
Darkstariano <72635007+Darkstariano@users.noreply.github.com>
2021-10-31 03:13:41 -0500

    Mejorado

---
+++ Akriano Remake/Assets/Nuevos Scripts/Nuevos Scripts/Enemigos/Enemigos/Bala.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public string EnemyName;
+    public float healthPoints;
+    public float damageToGive;
+}
---
+++ Akriano Remake/Assets/Nuevos Scripts/Nuevos Scripts/Enemigos/Enemigos/EnemyLarge.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLarge : MonoBehaviour
+{
+    public string EnemyName;
+    public float EnemyhealthPoints;
+    public float damageToGive;
+    public float knockbackForceX;
+    public float knockbackForceY;
+}
